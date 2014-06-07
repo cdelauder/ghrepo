b0d406bef402869a0e8dd8ac23615b2973d486c6
Apprentice <apprentice@dbc14.local>
2014-06-07 16:39:40 -0700

    can now create a rails framework and push it to github master for you

--- lib/ghrepo.rb
+++ lib/ghrepo.rb
@@ -15,7 +15,7 @@ module Ghrepo
       response = `curl -u "#{credentials[:username]}:#{credentials[:password]}" https://api.github.com/user/repos -d '{"name":"'#{repo_name}'"}'`
       git_url = JSON.parse(response)[credentials[:url]]
 
-      `git clone "#{git_url}"`
+      args.include?('-rails') ? include_rails(args, repo_name, git_url) : `git clone "#{git_url}"`
     else
       puts "RTFM dummy!"
       puts <<-eos
@@ -44,12 +44,12 @@ module Ghrepo
   end
 
   def prompt_password
-    puts "password > "
+    print "password > "
     password = STDIN.noecho(&:gets).chomp
   end
 
   def set_url(args)
-    args.include?('-ssl') ? 'ssh_url' : 'git_url'
+    args.include?('-ssl') ? 'ssh_url' : 'clone_url'
   end
 
   def set_username
@@ -57,7 +57,7 @@ module Ghrepo
   end
 
   def prompt_username
-    puts "github username > "
+    print "github username > "
     username = STDIN.gets.chomp
   end
 
@@ -67,5 +67,24 @@ module Ghrepo
 
   def set_credentials(args)
     {url: set_url(args), username: set_username, password: set_password}
+  end
+
+  def include_rails(args, repo_name, git_url)
+    add_rails(repo_name, git_url)
+    push_rails
+  end
+
+  def add_rails(repo_name, git_url)
+    puts "\nbuilding your rails app...."
+    `rails new "#{repo_name}"`
+    Dir.chdir(repo_name)
+    `git init`
+    `git remote add origin "#{git_url}"`
+  end
+
+  def push_rails
+    `git add .`
+    `git commit -m "boilerplate rails"`
+    `git push -u origin master`
   end
 end
