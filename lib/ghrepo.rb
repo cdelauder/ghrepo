2b75e53a0e59cf4509400a6d5e1b09bfe13eb809
Apprentice <apprentice@dbc14.local>
2014-06-07 14:44:59 -0700

    User can create GH repo and clone it to their computer

--- lib/ghrepo.rb
+++ lib/ghrepo.rb
@@ -1,5 +1,71 @@
+# encoding: utf-8
+
 require "ghrepo/version"
+require "json"
+require "io/console"
 
 module Ghrepo
-  # Your code goes here...
+
+  extend self
+
+  def start(args)
+    if args.any?
+      repo_name = args.pop
+      credentials = set_credentials(args)
+      response = `curl -u "#{credentials[:username]}:#{credentials[:password]}" https://api.github.com/user/repos -d '{"name":"'#{repo_name}'"}'`
+      git_url = JSON.parse(response)[credentials[:url]]
+
+      `git clone "#{git_url}"`
+    else
+      puts "RTFM dummy!"
+      puts <<-eos
+        ░░░░░░░░░▄░░░░░░░░░░░░░░▄░░░░
+        ░░░░░░░░▌▒█░░░░░░░░░░░▄▀▒▌░░░
+        ░░░░░░░░▌▒▒█░░░░░░░░▄▀▒▒▒▐░░░
+        ░░░░░░░▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐░░░
+        ░░░░░▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐░░░
+        ░░░▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌░░░
+        ░░▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒▌░░
+        ░░▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐░░
+        ░▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌░
+        ░▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌░
+        ▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒▐░
+        ▐▒▒▐▀▐▀▒░▄▄▒▄▒▒▒▒▒▒░▒░▒░▒▒▒▒▌
+        ▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▐░
+        ░▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒░▒░▒░▒░▒▒▒▌░
+        ░▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▄▒▒▐░░
+        ░░▀▄▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▄▒▒▒▒▌░░
+        ░░░░▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀░░░
+        ░░░░░░▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀░░░░░
+        ░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▀▀░░░░░░░░
+      eos
+      puts "such moron very dumb"
+    end
+  end
+
+  def prompt_password
+    puts "password > "
+    password = STDIN.noecho(&:gets).chomp
+  end
+
+  def set_url(args)
+    args.include?('-ssl') ? 'ssh_url' : 'git_url'
+  end
+
+  def set_username
+    ENV['GHREPO_USERNAME'] || prompt_username
+  end
+
+  def prompt_username
+    puts "github username > "
+    username = STDIN.gets.chomp
+  end
+
+  def set_password
+    ENV['GHREPO_KEY'] || prompt_password
+  end
+
+  def set_credentials(args)
+    {url: set_url(args), username: set_username, password: set_password}
+  end
 end
